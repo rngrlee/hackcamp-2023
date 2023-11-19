dfa83edad9d87dc2545ecc3482824317acd20a1d
yood2 <yood2@proton.me>
2023-11-19 10:40:14 -0800

    Created Map component, Created subfolder containing Map css/js files

---
+++ universtay/src/components/Map.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect } from 'react';
+import 'mapFiles/jquery-jvectormap-2.0.5.css';
+import $ from 'jquery';
+import 'mapFiles/jquery-jvectormap-2.0.5.min.js';
+import 'map/Filesjquery-jvectormap-europe-merc.js';
+
+const JVectorMapComponent = () => {
+  useEffect(() => {
+    $(function () {
+      $('#world-map').vectorMap({
+        map: 'europe_merc',
+        onRegionClick: function (event, code) {
+          console.log('Success! ' + code + ' clicked.');
+        },
+      });
+    });
+  }, []); // Empty dependency array to ensure useEffect runs only once
+
+  return (
+    <div id="world-map" style={{ width: '600px', height: '400px' }}></div>
+  );
+};
+
+export default JVectorMapComponent;
